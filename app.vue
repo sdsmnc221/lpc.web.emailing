7e282c93e50ad6f90852f42b8d3eb8c31fda12c1
sdsmnc221 <antr.2201@gmail.com>
2025-06-16 15:51:45 +0200

    feat(app): init ssr tailwinds scss

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="scss"></style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="flex items-center justify-center h-screen bg-gray-100">
+    <h1 class="text-3xl">Hello Emailing</h1>
+  </main>
+</template>
